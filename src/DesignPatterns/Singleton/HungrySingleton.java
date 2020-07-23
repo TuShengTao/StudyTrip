99a85d4240fbb1ddfbe0233e97081cfd01b3a8f0
TuShengTao <1450190944@qq.com>
2020-07-23 02:40:47 +0000

    在Debian10测试

--- src/DesignPatterns/Singleton/HungrySingleton.java
+++ src/DesignPatterns/Singleton/HungrySingleton.java
@@ -1,9 +1,18 @@
+/*
+ * @Author: 屠圣涛 
+ * @Date: 2020-07-23 02:39:27
+ * @LastEditTime: 2020-07-23 02:39:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /studySpace/StudyTrip/src/DesignPatterns/Singleton/HungrySingleton.java
+ */ 
 /**
  * Created on 2020/6/3
  *
  * @author Tu ShengTao
  * Description: 单例模式之饿汉形式
  * 饿汉形式：在该类被加载进jvm的时候就创建该实例,只会被加载一次
+ * 
  */
 public class HungrySingleton {
     private static final HungrySingleton singleton = new HungrySingleton();
